c0e5cc5318058d9ccf3b8a020aca3d89d5f901d2
MAB <20094576@mail.wit.ie>
2024-11-13 10:56:59 +0100

    err

--- src/routes/service/+page.svelte
+++ src/routes/service/+page.svelte
@@ -2,27 +2,32 @@
     import { onMount } from 'svelte';
   
     let features = []; 
-  
+    let errorMessage = '';
+
     onMount(async () => {
       try {
         const response = await fetch('https://gis.geosphere.at/maps/rest/services/Test/geoinformation_services_website/MapServer/2/query?where=1%3D1&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson');
       
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          const errorText = await response.text(); 
+          throw new Error(`Network response was not ok: ${errorText}`);
         }
   
         const data = await response.json();
         features = data.features; 
       } catch (error) {
         console.error('Error fetching data:', error);
+        errorMessage = error.message; 
       }
     });
-  </script>
+</script>
 
-  <main>
-   <div class="container mx-auto my-24">
+<main>
+  <div class="container mx-auto my-24">
     <h1 class="mb-4 text-3xl">Service Information</h1>
-    {#if features.length > 0}
+    {#if errorMessage}
+      <p class="text-red-500">Error: {errorMessage}</p>
+    {:else if features.length > 0}
       <ul>
         {#each features as feature}
           <li class="mb-4">
@@ -40,6 +45,5 @@
     {:else}
       <p>No features found.</p>
     {/if}
-    </div>
-  </main>
-  
+  </div>
+</main>

8cdf32d4fb2b6767c24b96a3ff1f21c4d18eb889
MAB <20094576@mail.wit.ie>
2024-11-13 13:45:17 +0100

    rm vercel

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Cr1cKpN6.js","_app/immutable/chunks/scheduler.D2WwRDck.js","_app/immutable/chunks/index.B-65el-e.js","_app/immutable/chunks/stores.CsP4aPRx.js","_app/immutable/chunks/entry.C2Zxd785.js","_app/immutable/chunks/index.DGKpnwbj.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/chunks/en.BuA82QFa.js
+++
@@ -1,1 +0,0 @@
-const e="Hello",l="Goodbye",o={greeting:e,farewell:l};export{o as default,l as farewell,e as greeting};
